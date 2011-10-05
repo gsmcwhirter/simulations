8ba91dfce4387f894e5cda088737c459cd0eed6e
Gregory McWhirter <greg@hallofkvasir.org>
2011-10-05 12:37:52 -0700

    Removing attempts at catching KeyboardInterrupt... just ctrl-z, kill % when it hangs.

--- src/gametheory/base/simulation.py
+++ src/gametheory/base/simulation.py
@@ -4,7 +4,6 @@ __date__ ="$Sep 27, 2011 4:23:03 PM$"
 import cPickle
 import multiprocessing as mp
 
-#from contextlib import contextmanager
 from optparse import OptionParser
 
 effective_zero_diff = 1e-11
@@ -23,19 +22,6 @@ class Simulation:
         self._setParserOptions()
         self._runSimulation = runSimulation
 
-    #@contextmanager
-    #def __mpPool(self, size, quiet):
-    #    try:
-    #        pool = mp.Pool(size)
-    #        if not quiet:
-    #            print "Pool Started: {0}".format(pool)
-    #        yield pool
-    #    finally:
-    #        pool.close()
-    #        pool.terminate()
-    #        print "Terminated by Interrupt!"
-
-
     def go(self):
         (self._options, self._args) = self._oparser.parse_args()
         self.__checkBaseParserOptions()
@@ -46,45 +32,40 @@ class Simulation:
 
         stats = open(output_base.format(self._options.stats_file), "wb")
 
-        #with self.__mpPool(self._options.pool_size, self._options.quiet) as pool:
-        try:
-            pool = mp.Pool(self._options.pool_size)
+        pool = mp.Pool(self._options.pool_size)
+        if not self._options.quiet:
+            print "Pool Started: {0}".format(pool)
+
+        mp.log_to_stderr()
+
+        if not self._options.quiet:
+            print "Running {0} duplications.".format(self._options.dup)
+
+        task_base = self._buildTask()
+        task_extras = [self._options.skip, self._options.quiet]
+
+        if self._task_dup_num and self._options.file_dump:
+            tasks = [tuple([i] + task_base + [output_base.format(self._options.output_file.format(i + 1))] + task_extras) for i in range(self._options.dup)]
+        elif self._task_dup_num:
+            tasks = [tuple([i] + task_base + [None] + task_extras) for i in range(self._options.dup)]
+        elif self._options.file_dump:
+            tasks = [tuple(task_base + [output_base.format(self._options.output_file.format(i + 1))] + task_extras) for i in range(self._options.dup)]
+        else:
+            tasks = [tuple(task_base + [None] + task_extras)] * self._options.dup
+
+        results = pool.imap_unordered(self._runSimulation, tasks)
+        finished_count = 0
+        print >>stats, cPickle.dumps(self._options)
+        print >>stats
+        for result in results:
+            finished_count += 1
             if not self._options.quiet:
-                print "Pool Started: {0}".format(pool)
-
-            mp.log_to_stderr()
-
-            if not self._options.quiet:
-                print "Running {0} duplications.".format(self._options.dup)
-
-            task_base = self._buildTask()
-            task_extras = [self._options.skip, self._options.quiet]
-
-            if self._task_dup_num and self._options.file_dump:
-                tasks = [tuple([i] + task_base + [output_base.format(self._options.output_file.format(i + 1))] + task_extras) for i in range(self._options.dup)]
-            elif self._task_dup_num:
-                tasks = [tuple([i] + task_base + [None] + task_extras) for i in range(self._options.dup)]
-            elif self._options.file_dump:
-                tasks = [tuple(task_base + [output_base.format(self._options.output_file.format(i + 1))] + task_extras) for i in range(self._options.dup)]
-            else:
-                tasks = [tuple(task_base + [None] + task_extras)] * self._options.dup
-
-            results = pool.imap_unordered(self._runSimulation, tasks)
-            finished_count = 0
-            print >>stats, cPickle.dumps(self._options)
+                print self._formatRun(result)
+            print >>stats, cPickle.dumps(result)
             print >>stats
-            for result in results:
-                finished_count += 1
-                if not self._options.quiet:
-                    print self._formatRun(result)
-                print >>stats, cPickle.dumps(result)
-                print >>stats
-                stats.flush()
-                print "done #{0}".format(finished_count)
-        finally:
-            pool.close()
-            pool.terminate()
-            print "Terminated Process Pool!"
+            stats.flush()
+            print "done #{0}".format(finished_count)
+
 
         stats.close()
         self._whenDone()
