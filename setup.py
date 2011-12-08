4673484bd2aa9a617bd06c54565689718e318f92
Gregory McWhirter <greg@hallofkvasir.org>
2011-12-08 14:23:58 -0800

    Updates to various things -- mostly trying to set things up for pip instead of easy_install

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
-__author__="gmcwhirt"
-__date__ ="$Sep 26, 2011 2:43:53 PM$"
+#!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup (
-  name = 'gametheory.base',
-  version = '0.1',
-  packages = find_packages('src'),
-  package_dir = {
-    '': 'src',
-  },
-  namespace_packages = ['gametheory'],
-  author = 'Gregory McWhirter',
-  author_email = 'gmcwhirt@uci.edu',
-  description = 'Game theory simulations for escalation research',
-  url = 'https://www.github.com/gsmcwhirter/gametheory',
-  license = 'MIT',
-  entry_points = {
-    "console_scripts": [
-    ]
-  }
+    name = 'gametheory.base',
+    version = '0.1',
+    packages = [
+        'gametheory.base'
+    ],
+    package_dir = {
+        '': 'src',
+    },
+    author = 'Gregory McWhirter',
+    author_email = 'gmcwhirt@uci.edu',
+    description = 'Game theory simulations base package',
+    url = 'https://www.github.com/gsmcwhirter/gametheory',
+    license = 'MIT'
 )
