678a67c4d272182b1f0f80d839da53c2fd657de8
Greg McWhirter <greg@hallofkvasir.org>
2011-12-09 01:31:20 +0000

    Fixing setup.py errors and how to manage requirements for pip

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ setup (
     package_dir = {
         '': 'src',
     },
-    requires = [
-        "numpy >= 1.5"
-    ],
     author = 'Gregory McWhirter',
     author_email = 'gmcwhirt@uci.edu',
     description = 'Game theory simulations base package',
