391c18c834b09a45271d9a497622b907ed447810
Gregory McWhirter <greg@hallofkvasir.org>
2012-01-19 14:56:31 -0800

    Bumping version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup (
     name = 'gametheory.base',
-    version = '0.3.0',
+    version = '0.3.1',
     packages = [
         'gametheory',
         'gametheory.base'
