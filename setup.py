a69bb47a26df3a60f10ba1bc38a5124dce9e3f39
Gregory McWhirter <greg@hallofkvasir.org>
2012-02-03 15:11:21 -0800

    Cleaning up setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ from setuptools import setup
 setup(
     name='simulations',
     version='0.5.0',
+    author='Gregory McWhirter',
+    author_email='gmcwhirt@uci.edu',
+    description='A framework for evolutionary game theory simulations',
+    url='https://www.github.com/gsmcwhirter/simulations',
+    license='MIT',
     packages=[
         'simulations',
         'simulations.utils',
@@ -17,16 +22,11 @@ setup(
         '': 'src',
     },
     install_requires=[
-        'distribute',
         'numpy>=1.5',
         'pp'
     ],
-    dependency_links=["https://www.ideafreemonoid.org/pip"],
-    test_suite='nose.collector',
-    tests_require=['nose'],
-    author='Gregory McWhirter',
-    author_email='gmcwhirt@uci.edu',
-    description='A framework for evolutionary game theory simulations',
-    url='https://www.github.com/gsmcwhirter/gametheory',
-    license='MIT'
+    tests_require=[
+        'nose>=1.0'
+    ],
+    test_suite='nose.collector'
 )
