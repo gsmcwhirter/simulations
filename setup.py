65b7819f2214787349181fb05ad41b0aac0adbd4
Gregory McWhirter <greg@hallofkvasir.org>
2012-02-15 17:48:44 -0800

    Bumping version numbers

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import Extension
 
 setup(
     name='simulations',
-    version='0.6.7',
+    version='0.7.0',
     author='Gregory McWhirter',
     author_email='gmcwhirt@uci.edu',
     description='A framework for evolutionary game theory simulations',
